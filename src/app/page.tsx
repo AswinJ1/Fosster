13ba63a17ca34ece092caf1d55b04787985b85cf
AswinJ1 <aswinjhome@gmail.com>
2025-07-02 12:15:15 +0530

    fixing issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,21 @@
+// app/page.tsx
 'use client';
 
 import React from "react";
 import { Toaster } from "@components/ui/sonner";
 import { TooltipProvider } from "@components/ui/tooltip";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import DiscordFOSSter from "pages/discord-fosster";
-import NotFound from "pages/not-found";
+import Home from "@components/home";
 
 const queryClient = new QueryClient();
 
-function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<DiscordFOSSter />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
-
-function App() {
+export default function Page() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </BrowserRouter>
+      <TooltipProvider>
+        <Toaster />
+        <Home />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
-
-export default App;
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,3 +1,5 @@
+// src/components/Home.tsx
+'use client';
 import FOSSterHeader from "@components/fosster-header";
 import DiscordHero from "@components/discord-hero";
 import DiscordSection1 from "@components/discord-section-1";
@@ -6,21 +8,20 @@ import DiscordSection3 from "@components/discord-section-3";
 import DiscordSection4 from "@components/discord-section-4";
 import DiscordSection5 from "@components/discord-section-5";
 import DiscordFooter from "@components/discord-footer";
-// import Marquee from "@components/marquee";
 import Footer from "@components/footer";
-export default function DiscordFOSSter() {
+
+export default function Home() {
   return (
     <div className="min-h-screen">
       <FOSSterHeader />
       <DiscordHero />
       <DiscordSection2 />
       <DiscordSection1 />
-      {/* <Marquee /> */}
       <DiscordSection3 />
       <DiscordSection4 />
       <DiscordSection5 />
       <DiscordFooter />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
